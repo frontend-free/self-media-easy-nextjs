186b085d1931662331c4f034aa7a4e820d27d1ea
liyatang <liyatang@qq.com>
2025-06-03 14:40:25 +0800

    feat: some

--- src/app/actions/auth_actions.ts
+++ src/app/actions/auth_actions.ts
@@ -43,7 +43,7 @@ export async function getUserByNamePassword({
 }) {
   return wrapServerAction(async () => {
     return prisma.user.findUnique({
-      where: { name, password },
+      where: { name, password, deletedAt: null },
     });
   });
 }
